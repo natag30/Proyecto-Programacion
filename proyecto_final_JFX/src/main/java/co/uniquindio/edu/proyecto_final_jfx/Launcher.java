3bae7a2240b898065c29aa5ba02d6020c43d974b
natag30 <149449326+natag30@users.noreply.github.com>
2026-05-15 18:23:34 -0500

    update

---
+++ proyecto_final_JFX/src/main/java/co/uniquindio/edu/proyecto_final_jfx/Launcher.java
@@ -0,0 +1,12 @@
+package co.uniquindio.edu.proyecto_final_jfx;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
--- src/co/uniquindio/edu/poo/PlataformaEventos.java
+++
@@ -1,5 +0,0 @@
-package co.uniquindio.edu.poo;
-
-public class PlataformaEventos {
-
-}
