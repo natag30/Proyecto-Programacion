4e22143e94551bc777433abf0b1e5e0f2ee770d0
natag30 <149449326+natag30@users.noreply.github.com>
2026-05-17 13:40:05 -0500

    u

--- proyecto_final_JFX/src/main/java/co/uniquindio/edu/proyecto_final_jfx/Launcher.java
+++ proyecto_final_JFX/src/main/java/co/uniquindio/edu/proyecto_final_jfx/Launcher.java
@@ -1,11 +1,30 @@
 package co.uniquindio.edu.proyecto_final_jfx;
 
+import co.uniquindio.edu.proyecto_final_jfx.model.patrones.estructurales.adapter.*;
+import co.uniquindio.edu.proyecto_final_jfx.model.patrones.estructurales.decorator.*;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
 
+        IEntrada entrada = new EntradaBase("Entrada Base", 100000);
+        entrada = new AccesoVIPDecorator(entrada);
+        entrada = new SeguroCancelacionDecorator(entrada);
+        entrada = new MerchandisingDecorator(entrada);
+        System.out.println(entrada.getDescription());
+        System.out.println(entrada.getPrecio());
 
+        PSE pse = new PSE();
+
+        IMetodoPago pagoPSE = new PSEAdapter(pse);
+
+        pagoPSE.realizarPago(100000);
+
+        TarjetaCredito tarjeta = new TarjetaCredito();
+
+        IMetodoPago pagoTarjeta = new TarjetaAdapter(tarjeta);
+
+        pagoTarjeta.realizarPago(100000);
 
 
     }
--- proyecto_final_JFX/src/main/java/co/uniquindio/edu/proyecto_final_jfx/model/patrones/estructurales/adapter/PSEAdapter.java
+++ proyecto_final_JFX/src/main/java/co/uniquindio/edu/proyecto_final_jfx/model/patrones/estructurales/adapter/PSEAdapter.java
@@ -1,4 +1,18 @@
 package co.uniquindio.edu.proyecto_final_jfx.model.patrones.estructurales.adapter;
 
-public class PSEAdapter implements IMetodoPago{
+public class PSEAdapter implements IMetodoPago {
+
+    private PSE pse;
+
+    public PSEAdapter(PSE pse) {
+        this.pse = pse;
+    }
+
+    @Override
+    public void realizarPago(double monto) {
+        String amount = String.valueOf(monto);
+        pse.sendPayment(amount, "COP");
+        System.out.println("Pagando " + monto + " con PSE.");
+    }
+
 }
