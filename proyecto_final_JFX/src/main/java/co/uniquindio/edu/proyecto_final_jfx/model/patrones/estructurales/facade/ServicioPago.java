3199e55ff0de86374791f6c8ac9409b691f87d38
natag30 <149449326+natag30@users.noreply.github.com>
2026-05-17 19:51:48 -0500

    facade

---
+++ proyecto_final_JFX/src/main/java/co/uniquindio/edu/proyecto_final_jfx/model/patrones/estructurales/facade/ServicioPago.java
@@ -0,0 +1,17 @@
+package co.uniquindio.edu.proyecto_final_jfx.model.patrones.estructurales.facade;
+
+import co.uniquindio.edu.proyecto_final_jfx.model.compra.Pago;
+
+public class ServicioPago {
+
+    public boolean procesarPago(Pago pago){
+        if(pago.getValor()<=0){
+            System.out.println("El Pago no ha sido realizado");
+            return false;
+        }
+        System.out.println("El Pago ha sido realizado Correctamente");
+        System.out.println("Pago a realizar :"+pago.getValor());
+
+        return true;
+    }
+}
